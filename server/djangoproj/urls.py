1cd6fe39bf74a01da42273800a7fcb72d332927d
ibeezuma <ibeezuma@gmail.com>
2024-03-05 20:16:18 -0500

    Adding temporary changes to Github

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,6 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,4 +1,6 @@
 import LoginPanel from "./components/Login/Login"
+import PostReview from "./components/Dealers/PostReview"
+import Dealer from "./components/Dealers/Dealer"
 import Dealers from './components/Dealers/Dealers';
 import { Routes, Route } from "react-router-dom";
 
@@ -7,6 +9,8 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
