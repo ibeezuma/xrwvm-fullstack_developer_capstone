34e733b1f04c6b76d03beae58b6aa7519353a58f
ibeezuma <ibeezuma@gmail.com>
2024-03-05 20:07:45 -0500

    Adding temporary changes to Github

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
