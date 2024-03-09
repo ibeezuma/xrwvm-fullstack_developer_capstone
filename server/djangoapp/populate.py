e1cede809b0d54ea75760211f70e4109fbae19fd
ibeezuma <154035093+ibeezuma@users.noreply.github.com>
2024-03-09 11:18:28 -0700

    populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -2,7 +2,6 @@ def initiate():
     print("Populate not implemented. Add data manually")
 from .models import CarMake, CarModel
 
-def initiate():
     car_make_data = [
         {"name": "NISSAN", "description": "Great cars. Japanese technology"},
         {"name": "Mercedes", "description": "Great cars. German technology"},
@@ -13,7 +12,7 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-            car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
+            car_make_instances.append(CarMake.objects.create(name = data['name'], description = data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -36,4 +35,4 @@ def initiate():
     ]
 
     for data in car_model_data:
-            CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+            CarModel.objects.create(name = data['name'], car_make = data['car_make'], type = data['type'], year = data['year'])
