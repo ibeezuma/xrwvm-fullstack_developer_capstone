c13b271f909b78cbf86e8062a2783adcd6ce0a22
ibeezuma <154035093+ibeezuma@users.noreply.github.com>
2024-03-09 15:11:02 -0700

    urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,32 +10,32 @@ urlpatterns = [
                   # # path for registration
 
                   # path for login
-                  path(route='login', view=views.login_user, 
+                  path(route='login', view=views.login_user,
                        name='login'),
 
                   # path for logout
-                  path(route='logout', view=views.logout, 
+                  path(route='logout', view=views.logout,
                        name='logout'),
 
                   # path for dealer reviews view
-                  path(route='get_dealers', 
-                       view=views.get_dealerships, 
+                  path(route='get_dealers',
+                       view=views.get_dealerships,
                        name='get_dealers'),
                   path(route='get_dealers/<str:state>',
-                       view=views.get_dealerships, 
+                       view=views.get_dealerships,
                        name='get_dealers_by_state'),
-                  path(route='dealer/<int:dealer_id>', 
-                       view=views.get_dealer_details, 
+                  path(route='dealer/<int:dealer_id>',
+                       view=views.get_dealer_details,
                        name='dealer_details'),
-                  path(route='reviews/dealer/<int:dealer_id>', 
-                       view=views.get_dealer_reviews, 
+                  path(route='reviews/dealer/<int:dealer_id>',
+                       view=views.get_dealer_reviews,
                        ame='dealer_details'),
                   # path for add a review view
                   path(route='add_review', view=views.add_review,
                        name='add_review'),
-                  path(route='get_cars', view=views.get_cars, 
+                  path(route='get_cars', view=views.get_cars,
                        name='getcars'),
-              ] + static(settings.MEDIA_URL, 
+              ] + static(settings.MEDIA_URL,
                          document_root=settings.MEDIA_ROOT)
 
                 
