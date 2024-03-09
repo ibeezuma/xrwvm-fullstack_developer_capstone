0a340bf45939d515cb08d0e31f691e5fc51a16a9
ibeezuma <154035093+ibeezuma@users.noreply.github.com>
2024-03-09 14:46:23 -0700

    urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,10 +10,12 @@ urlpatterns = [
                   # # path for registration
 
                   # path for login
-                  path(route='login', view=views.login_user, name='login'),
+                  path(route='login', view=views.login_user, 
+                       name='login'),
 
                   # path for logout
-                  path(route='logout', view=views.logout, name='logout'),
+                  path(route='logout', view=views.logout, 
+                       name='logout'),
 
                   # path for dealer reviews view
                   path(route='get_dealers', 
@@ -33,4 +35,7 @@ urlpatterns = [
                        name='add_review'),
                   path(route='get_cars', view=views.get_cars, 
                        name='getcars'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, 
+                         document_root=settings.MEDIA_ROOT)
+
+                
