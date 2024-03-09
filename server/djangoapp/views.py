68db16d02a2a9f131aeba4249aaad00aa22bfc6a
ibeezuma <154035093+ibeezuma@users.noreply.github.com>
2024-03-09 15:18:59 -0700

    views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,4 +1,5 @@
 # Uncomment the required imports before adding the code
+import requests
 from .models import CarMake, CarModel
 from django.contrib.auth import login, authenticate
 import logging
@@ -33,13 +34,13 @@ def login_user(request):
     username = data['userName']
     password = data['password']
     # Try to check if provide credential can be authenticated
-    user = authenticate(username=username, 
+    user = authenticate(username=username,
                         password=password)
     data = {"userName": username}
     if user is not None:
         # If user is valid, call login method to login current user
         login(request, user)
-        data = {"userName": username, 
+        data = {"userName": username,
                 "status": "Authenticated"}
     return JsonResponse(data)
 
@@ -70,18 +71,18 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, 
-                                        first_name=first_name, 
+        user = User.objects.create_user(username=username,
+                                        first_name=first_name,
                                         last_name=last_name,
-                                        password=password, 
+                                        password=password,
                                         email=email)
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName": username, 
+        data = {"userName": username,
                 "status": "Authenticated"}
         return JsonResponse(data)
     else:
-        data = {"userName": username, 
+        data = {"userName": username,
                 "error": "Already Registered"}
         return JsonResponse(data)
 
@@ -105,10 +106,10 @@ def get_dealer_reviews(dealer_id):
                 review_detail['review'])
             print(response)
             review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status": 200, 
+        return JsonResponse({"status": 200,
                              "reviews": reviews})
     else:
-        return JsonResponse({"status": 400, 
+        return JsonResponse({"status": 400,
                              "message": "Bad Request"})
 
 
@@ -119,7 +120,7 @@ def get_dealer_details(dealer_id):
         return JsonResponse({"status": 200, 
                              "dealer": dealership})
     else:
-        return JsonResponse({"status": 400, 
+        return JsonResponse({"status": 400,
                              "message": "Bad Request"})
 
 
@@ -129,11 +130,8 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception as e:
             print(f"Error: {e}")
-            return JsonResponse({"status": 401, 
+            return JsonResponse({"status": 401,
                                  "message": "Error in posting review"})
     else:
-        return JsonResponse({"status": 403, 
+        return JsonResponse({"status": 403,
                              "message": "Unauthorized"})
-    
-
-    
