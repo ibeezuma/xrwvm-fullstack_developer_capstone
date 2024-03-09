6d2377fcbc7d2f3b08802e32609f2a28ed0a55a5
ibeezuma <154035093+ibeezuma@users.noreply.github.com>
2024-03-09 14:26:53 -0700

    views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -21,7 +21,8 @@ def get_cars():
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -32,12 +33,14 @@ def login_user(request):
     username = data['userName']
     password = data['password']
     # Try to check if provide credential can be authenticated
-    user = authenticate(username=username, password=password)
+    user = authenticate(username=username, 
+                        password=password)
     data = {"userName": username}
     if user is not None:
         # If user is valid, call login method to login current user
         login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
+        data = {"userName": username, 
+                "status": "Authenticated"}
     return JsonResponse(data)
 
 
@@ -67,14 +70,19 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
-                                        password=password, email=email)
+        user = User.objects.create_user(username=username, 
+                                        first_name=first_name, 
+                                        last_name=last_name,
+                                        password=password, 
+                                        email=email)
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
+        data = {"userName": username, 
+                "status": "Authenticated"}
         return JsonResponse(data)
     else:
-        data = {"userName": username, "error": "Already Registered"}
+        data = {"userName": username, 
+                "error": "Already Registered"}
         return JsonResponse(data)
 
 
@@ -93,21 +101,26 @@ def get_dealer_reviews(dealer_id):
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
+            response = analyze_review_sentiments(
+                review_detail['review'])
             print(response)
             review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status": 200, "reviews": reviews})
+        return JsonResponse({"status": 200, 
+                             "reviews": reviews})
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
+        return JsonResponse({"status": 400, 
+                             "message": "Bad Request"})
 
 
 def get_dealer_details(dealer_id):
     if dealer_id:
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
-        return JsonResponse({"status": 200, "dealer": dealership})
+        return JsonResponse({"status": 200, 
+                             "dealer": dealership})
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
+        return JsonResponse({"status": 400, 
+                             "message": "Bad Request"})
 
 
 def add_review(request):
@@ -116,7 +129,11 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception as e:
             print(f"Error: {e}")
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse({"status": 401, 
+                                 "message": "Error in posting review"})
     else:
-        return JsonResponse({"status": 403, "message": "Unauthorized"})
+        return JsonResponse({"status": 403, 
+                             "message": "Unauthorized"})
+    
+
     
