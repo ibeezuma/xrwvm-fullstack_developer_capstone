161d76ded65440250fc1e767ff5a01e390c9f207
ibeezuma <154035093+ibeezuma@users.noreply.github.com>
2024-03-09 18:13:02 -0700

    app.js

--- server/database/app.js
+++ server/database/app.js
@@ -92,12 +92,12 @@ app.get('/fetchDealers/:id', async (req, res) => {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
   const documents = await Reviews.find().sort( { id: -1 } );
-  let new_id = documents.0.id +1
+ 
 
   const review = new Reviews({
 		"id": new_id,
 		"name": data.name,
-		"dealership": data['dealership'],
+		"dealership": data.dealership,
 		"review": data.review,
 		"purchase": data.purchase,
 		"purchase_date": data.purchase_date,
