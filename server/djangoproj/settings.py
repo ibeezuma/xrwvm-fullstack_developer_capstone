67997e5ae87ba7691f6213dc5e2449519177c931
ibeezuma <154035093+ibeezuma@users.noreply.github.com>
2024-03-09 17:51:02 -0700

    settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,13 +27,14 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost',
-                 'https://ibeezuma-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = [
-    'https://ibeezuma-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-
+ALLOWED_HOSTS = ['localhost', 'https://ibeezuma-8000.theiadockernext'
+                              '-0-labs-prod-theiak8s-4-tor01.proxy'
+                              '.cognitiveclass.ai']
+SRF_TRUSTED_ORIGINS = ['https://ibeezuma-8000.theiadockernext-'
+                        '0-labs-prod-theiak8s-4-tor01.proxy'
+                        '.cognitiveclass.ai']
 REST_FRAMEWORK = {'DEFAULT_AUTHENTICATION_CLASSES': [],
-}
+                  }
 
 # Application definition
 
@@ -94,7 +95,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_'
+        'validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -142,4 +144,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static')]
-
